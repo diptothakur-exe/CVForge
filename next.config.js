3796123340ca452e826ab9e929a3fcd5c89d6c36
Dipto Thakur <dkt.officials@gmail.com>
2026-04-11 03:04:44 +0530

    initial 01

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverComponentsExternalPackages: ["pdf-parse", "mammoth"] }
-}
-module.exports = nextConfig
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d0015fe7eb41429ece3f6d0f1652e83e69d9892dc27ff395c0f52895be60181
+size 171
--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,3 @@
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } }
+version https://git-lfs.github.com/spec/v1
+oid sha256:68fac9e49f1efae93cbcb642ed05fd127f6caf05ecc0686e4aae3be93bbdb1e0
+size 68
